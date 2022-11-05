b0279d48a9e93d3c18926caf4616f92db4261b47
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-11-05 12:13:54 -0500

    Input ciudad 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,88 +23,105 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/dom.png',
-            height: 56.0,
-            color: Colors.white,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "26",
-                style: TextStyle(
-                  fontSize: 80.0,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                width: 4.0,
-              ),
-              Text(
-                "°C",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 12.0,
-          ),
-          TextFormField(
-            style: const TextStyle(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dom.png',
+              height: 56.0,
               color: Colors.white,
-              fontSize: 14.0,
             ),
-            decoration: InputDecoration(
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
-              hintText: "Ingresa una ciudad",
-              hintStyle: const TextStyle(
-                color: Colors.white60,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "26",
+                  style: TextStyle(
+                    fontSize: 80.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 4.0,
+                ),
+                Text(
+                  "°C",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 2.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Arequipa, Perú",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 18.0,
+            ),
+            TextFormField(
+              style: const TextStyle(
+                color: Colors.white,
                 fontSize: 14.0,
               ),
-              filled: true,
-              fillColor: Colors.white.withOpacity(0.12),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(14.0),
-                borderSide: BorderSide.none,
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(14.0),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 12.0,
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: 48.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xffFE6C6D),
-                shape: RoundedRectangleBorder(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 14.0),
+                hintText: "Ingresa una ciudad",
+                hintStyle: const TextStyle(
+                  color: Colors.white60,
+                  fontSize: 14.0,
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.12),
+                focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(14.0),
+                  borderSide: BorderSide.none,
                 ),
-              ),
-              child: Text(
-                "Buscar",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.w600,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14.0),
+                  borderSide: BorderSide.none,
                 ),
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 18.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 48.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xffFE6C6D),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14.0),
+                  ),
+                ),
+                child: Text(
+                  "Buscar",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
